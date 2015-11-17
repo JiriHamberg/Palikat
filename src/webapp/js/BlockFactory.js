e31c9be5178a5c57bf83fb12d55c69c923abad54
Piia <Piia@Piia-MBP.Elisa>
2015-11-17 23:40:31 +0200

    factory is ON

--- src/webapp/js/BlockFactory.js
+++ src/webapp/js/BlockFactory.js
@@ -5,43 +5,50 @@ var BlockFactory = (function() {
 
 	// tekee palikan 3D-mallin (matriisin, engl. matrix) mukaan
 	// palikka on mesh-olio
-	var make = function(matrix) {
+	var make = function(matrix, material) {
 
 		// Aloitetaan isomman mesh-kappaleen kokoaminen!
+
 	    // luodaan ensin mesh-lista, jonne lisätään kuutionmallisia mesh-kappaleita
 	    var meshes = [];
-	    var box;
+
+	    // jos materiaalia ei ole annettu, käytetään perusmateriaaliamme
+	    if(material === null) {
+	    	material = Config.BOX_MATERIAL;
+	    }
+
+	    // luodaan kuutio
+	    // tämän sijainti avaruudessa on (0, 0, 0)
+	    var box = new THREE.Mesh(Config.BOX_GEOMETRY, material);
+
+	    // käydään matriisia läpi
+	    // sijoitetaan kuutio matriisin osoittamaan paikkaan ja tallennetaan se listaan
 	    
-	    // luodaan box, lisätään se listaan
-	    box = new THREE.Mesh(geometry, material);
-	    meshes.push(box);
+	    for(var i = 0; i < matrix.length; i++) {
+	    	for(var j = 0; j < matrix[i].length; j++) {
+	    		for(var k = 0; k < matrix[i][j].length; k++) {
+	    			box.position.x = i * Config.UNIT_LENGTH;
+	    			box.position.y = j * Config.UNIT_LENGTH;
+	    			box.position.z = k * Config.UNIT_LENGTH;
+	    			meshes.push(box);
+	    		}
+	    	}
+	    }   
 
-	    // luodaan uusi box, lisätään sekin listaan
-	    box = new THREE.Mesh(geometry, material);
-	    box.position.x = Config.UNIT_LENGTH;
-	    meshes.push(box);
-
-	    box = new THREE.Mesh(geometry, material);
-	    box.position.y = 25;
-	    meshes.push(box);    
-
-	    // luodaan uusi geometrinen muoto, joka tulee olemaan kahden yhteenliitetyn kuution muotoinen
+	    // luodaan uusi geometrinen muoto, joka tulee olemaan yhteenliitettyjen kuutioiden muotoinen
 	    // tehdään muodon pohjalta mesh ja liitetään se näkymään
-	    mergeGeometry = mergeMeshes(meshes);
-	    mesh = new THREE.Mesh(mergeGeometry, material);
-
-	};
-
-	var mergeMeshes = function(meshes) {
-	    // luodaan uusi "geometrinen muoto", jota muokataan lisäämällä siihen uusia kappaleita .merge-funktiolla
-	    var combined = new THREE.Geometry();
-
+	    mergeGeometry = new THREE.Geometry();
 	    for (var i = 0; i < meshes.length; i++) {
 	        meshes[i].updateMatrix();
-	        combined.merge(meshes[i].geometry, meshes[i].matrix);
+	        mergeGeometry.merge(meshes[i].geometry, meshes[i].matrix);
 	    }
-	    return combined;
+
+	    // tehdään lopuksi uuden geometrian pohjalta mesh ja palautetaan se
+	    var mergeMesh = new THREE.Mesh(mergeGeometry, material);
+	    return mergeMesh;
+
 	};
+
 
 
 	return {
