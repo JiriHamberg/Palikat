06a42cf31d12352ca3d506a447f18c970fc77457
Jiri Hamberg <jiri.hamberg@cs.helsinki.fi>
2015-11-17 23:52:46 +0200

    Fix to Config.js

--- src/webapp/js/Config.js
+++ src/webapp/js/Config.js
@@ -2,9 +2,11 @@
 
 var Config = (function() {
 
+	var UNIT_LENGTH = 25;
+
 	return {
-		UNIT_LENGTH: 25,
-		BOX_GEOMETRY: new THREE.BoxGeometry( UNIT_LENGTH:, UNIT_LENGTH:, UNIT_LENGTH: ),
+		UNIT_LENGTH: UNIT_LENGTH,
+		BOX_GEOMETRY: new THREE.BoxGeometry( UNIT_LENGTH, UNIT_LENGTH, UNIT_LENGTH ),
     	BOX_MATERIAL: new THREE.MeshPhongMaterial( {color: 0xff0000 } )
 	};
 })();
