ea3f9f797a5fbed4d35571417faa089017981c57
Piia <piia.hartikka@helsinki.fi>
2018-10-28 18:51:35 +0200

    Added router to App

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Example from './Example';
 
+// normally the component assosiated to a route should be an independent container component
+const renderExample = () => <Example text="This is an example." />;
+
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Example text="This is an example." />
-      </div>
+	render() {
+		return (
+			<Router>
+				<div>
+					<Route exact path="/" component={ renderExample } />
+				</div>
+			</Router>
     );
-  }
+	}
 }
 
 export default App;
